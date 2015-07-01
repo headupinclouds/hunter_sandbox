6ccecdd4b41cbf4fae2720ff8675cc9d332d928b
David Hirvonen <dhirvonen@elucideye.com>
2015-07-01 10:38:10 -0400

    Added test_iostreams file

---
+++ test_iostreams.cpp
@@ -0,0 +1,36 @@
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <boost/iostreams/filtering_streambuf.hpp>
+#include <boost/iostreams/copy.hpp>
+#include <boost/iostreams/filter/zlib.hpp>
+
+void pack()
+{            
+    std::ofstream file("hello.z", std::ios_base::out | std::ios_base::binary);
+
+    boost::iostreams::filtering_streambuf<boost::iostreams::output> out;
+    out.push(boost::iostreams::zlib_compressor());
+
+    out.push(file);       
+    char data[5] = {'a', 'b', 'c', 'd', 'e'};    
+    boost::iostreams::copy(boost::iostreams::basic_array_source<char>(data, sizeof(data)), out);
+}
+
+void unpack()
+{
+    std::fstream file("hello.z", std::ios_base::in | std::ios_base::binary);
+
+    boost::iostreams::filtering_streambuf<boost::iostreams::input> in;
+    in.push(boost::iostreams::zlib_decompressor());
+
+    in.push(file);
+    boost::iostreams::copy(in, std::cout);
+}
+
+int main()
+{  
+    pack();
+    unpack();
+    return 0;
+}
